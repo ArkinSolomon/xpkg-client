0fc321386f9e731bdc39705fb5be134a94814a75
ArkinSolomon <arkin@arkinsolomon.net>
2022-09-27 08:48:53 -0400

    Make PRINT command tests better

--- src/test/java/net.xpkgclient/commands/PrintCommandTests.java
+++ src/test/java/net.xpkgclient/commands/PrintCommandTests.java
@@ -3,12 +3,13 @@ package net.xpkgclient.commands;
 import net.xpkgclient.ConfigSetupExtension;
 import net.xpkgclient.Configuration;
 import net.xpkgclient.ExecutionContext;
-import net.xpkgclient.ScriptExecutionHandler;
 import net.xpkgclient.exceptions.XPkgArgLenException;
 import net.xpkgclient.exceptions.XPkgImmutableVarException;
 import net.xpkgclient.exceptions.XPkgInvalidCallException;
 import net.xpkgclient.exceptions.XPkgUndefinedVarException;
+import net.xpkgclient.vars.XPkgBool;
 import net.xpkgclient.vars.XPkgString;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -19,13 +20,25 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//TODO Split the tests up into individual classes
 //Test the print command
 @ExtendWith(ConfigSetupExtension.class)
 class PrintCommandTests {
 
+    //Basic contexts that are shared among many tests
+    private static ExecutionContext context;
+
+    //Setup execution contexts
+    @BeforeAll
+    public static void setupBeforeClass() throws XPkgInvalidCallException, IOException, XPkgImmutableVarException {
+        context = ExecutionContext.createBlankContext();
+        context.setVar("$testStr", new XPkgString("Oh look at that a string"));
+        context.setVar("$test2", new XPkgBool(true));
+        context.setVar("$test3", new XPkgBool(false));
+    }
+
+
     // Get the string from executing a print test
-    private static String printText(String testArg, ExecutionContext context) throws Throwable {
+    private static String printText(String[] args, ExecutionContext context) throws Throwable {
 
         // Create a temporary print stream to save the output
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -34,7 +47,7 @@ class PrintCommandTests {
         PrintStream old = System.out;
         System.setOut(ps);
 
-        ScriptExecutionHandler.executeText("print " + testArg, context);
+        PrintCommand.execute(args, context);
 
         // Reset system.out
         System.out.flush();
@@ -42,48 +55,63 @@ class PrintCommandTests {
         return baos.toString();
     }
 
+    private static String printText(String[] args) throws Throwable {
+        return printText(args, null);
+    }
+
     // Print just regular strings
     @Test
-    void testBasicPrints() throws Throwable {
-        assertEquals("Hello", printText("Hello", null).trim());
-        assertEquals("Hello world!", printText("Hello world!", null).trim());
-        assertEquals("Howdy there ma'am", printText("Howdy there ma'am", null).trim());
+    void testPrintOneWord() throws Throwable {
+        assertEquals("Hello", printText(new String[]{"Hello"}));
     }
 
-    // Print some variables
     @Test
-    void testVariablePrints() throws Throwable {
-
-        // Setup test context
-        //TODO setup context using ExecutionContext methods instead of through scripts to isolate testing to the print command
-        ExecutionContext context = ExecutionContext.createBlankContext();
-        ScriptExecutionHandler.executeText("setstr $testStr Oh look at that a string", context);
-        ScriptExecutionHandler.executeText("set $test2 TRUE", context);
-        ScriptExecutionHandler.executeText("set $test3 FALSE", context);
-
-        assertEquals(" ", printText("$SPACE", context));
-        assertEquals("Oh look at that a string", printText("$testStr", context));
-        assertEquals("TRUE", printText("$test2", context));
-        assertEquals("FALSE", printText("$test3", context));
-        assertEquals(Configuration.getXpPath().toString(), printText("$XP_DIR", context));
+    void testPrintMultiWord() throws Throwable {
+        assertEquals("Hello world!", printText(new String[]{"Hello", "world!"}));
     }
 
-    // Test out exceptions
     @Test
-    void exceptions() throws XPkgInvalidCallException, XPkgImmutableVarException, IOException {
+    void testPrintApostrophe() throws Throwable {
+        assertEquals("Howdy there ma'am", printText(new String[]{"Howdy", "there", "ma'am"}));
+    }
 
-        //Setup test context
-        ExecutionContext context = ExecutionContext.createBlankContext();
-        context.setVar("$testStr", new XPkgString("Test string"));
+    @Test
+    void testPrintSpaceVariable() throws Throwable {
+        assertEquals(" ", printText(new String[]{"$SPACE"}, context));
+    }
 
-        // Test without any arguments
-        assertThrows(XPkgArgLenException.class, () -> ScriptExecutionHandler.executeText("print"));
-        assertThrows(XPkgArgLenException.class, () -> ScriptExecutionHandler.executeText("print   "));
+    @Test
+    void testPrintCustomVar() throws Throwable {
+        assertEquals("Oh look at that a string", printText(new String[]{"$testStr"}, context));
+    }
 
-        // Test with variable then text
-        assertThrows(XPkgArgLenException.class, () -> ScriptExecutionHandler.executeText("print $testStr hello"));
+    @Test
+    void testPrintBooleanTrue() throws Throwable {
+        assertEquals("TRUE", printText(new String[]{"$test2"}, context));
+    }
 
-        // Test with non-existent variable
-        assertThrows(XPkgUndefinedVarException.class, () -> ScriptExecutionHandler.executeText("print $nonExistentVar"));
+    @Test
+    void testPrintBooleanFalse() throws Throwable {
+        assertEquals("FALSE", printText(new String[]{"$test3"}, context));
+    }
+
+    @Test
+    void testPrintResource() throws Throwable {
+        assertEquals(Configuration.getXpPath().toString(), printText(new String[]{"$XP_DIR"}, context));
+    }
+
+    @Test
+    void testPrintNoArgs() {
+        assertThrows(XPkgArgLenException.class, () -> PrintCommand.execute(new String[]{}, context));
+    }
+
+    @Test
+    void testPrintVariableAndText() {
+        assertThrows(XPkgArgLenException.class, () -> PrintCommand.execute(new String[]{"$testStr", "hello"}, context));
+    }
+
+    @Test
+    void testPrintUndefinedVar() {
+        assertThrows(XPkgUndefinedVarException.class, () -> PrintCommand.execute(new String[]{"$nonExistentVar"}, context));
     }
 }
